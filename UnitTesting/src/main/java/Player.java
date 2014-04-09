c06e8b7775f98943197fae091ed5979bf6902a72
Bernice Zerafa <bernice.zerafa11@gmail.com>
2014-04-09 17:54:53 +0200

    Committed Player class with its associated Unit Tests

---
+++ UnitTesting/src/main/java/Player.java
@@ -0,0 +1,101 @@
+package main.java;
+
+import java.awt.Color;
+
+public class Player {
+
+	private Position pos;
+	private Position startingPosition;
+	private int[][] playerTrail;
+	
+	public Player()
+	{
+		
+	}
+	
+	public Position getStartingPosition() {
+		return startingPosition;
+	}
+	
+	public Position getPos() {
+		return pos;
+	}
+	
+	public boolean move(char direction, int size)
+	{
+		switch (direction)
+		{
+			case 'u':
+			case 'U': 
+				if(pos.getY() - 1 >= 0)
+					pos.setY(pos.getY() - 1); 
+				break; 
+			case 'd':
+			case 'D':
+				if(pos.getY() + 1 < size)
+					pos.setY(pos.getY() + 1); 
+				break;
+			case 'l':
+			case 'L':
+				if(pos.getX() - 1 >= 0)
+					pos.setX(pos.getX() - 1); 
+				break;
+			case 'r':
+			case 'R':
+				if(pos.getX() + 1 < size)
+					pos.setX(pos.getX() + 1); 
+				break;
+			default: return false;
+		}
+		return true;
+	}
+	
+	public void setPosition(Position pos)
+	{
+		// set player trail position to 1 if player opened a square
+		playerTrail[pos.getX()][pos.getY()] = 1;
+		this.pos = pos;
+	}
+	
+	public void setStartingPosition(Position startingPosition)
+	{
+		this.startingPosition = startingPosition;
+		this.pos = startingPosition;
+	}
+	
+	public void setStartingPosition(int size)
+	{
+		int randomRow;
+		int randomCol;
+		
+		Color thisColor = null;
+		
+		do
+		{
+			randomRow = (int) Math.floor(Math.random() * size);
+			randomCol = (int) Math.floor(Math.random() * size);
+			
+			thisColor = Map.getTiles()[randomRow][randomCol];
+			System.out.println(thisColor);
+		}
+		while (thisColor != Color.GREEN); ///// GREEN!!!!
+		
+		setTrail(size);
+		this.startingPosition = new Position(randomRow, randomCol);
+		this.pos = new Position(randomRow, randomCol);
+	}
+	
+	public void setTrail(int size)
+	{
+		playerTrail = new int[size][size];
+		
+		// init playerTrail
+		for (int i=0; i < playerTrail.length; i++)
+		{
+			for (int j=0; j < playerTrail.length; j++)
+			{
+				playerTrail[i][j] = 0;
+			}
+		}
+	}
+}
---
+++ UnitTesting/src/test/java/PlayerTests.java
@@ -0,0 +1,175 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Color;
+
+import main.java.Map;
+import main.java.Player;
+import main.java.Position;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTests {
+	
+	Map map = null;
+	Player player = null;
+	
+	@Before
+	public void before() {
+
+		map = new Map();
+		map.setSize(5);
+		map.generate();
+		player = new Player();
+		
+	} 
+	
+	@Test
+	public void moveTestLeftUppercase() {
+		
+		Position startPos = new Position(3, 4);
+		player.setStartingPosition(startPos);
+		Position checkPos = new Position(2, 4);
+		player.move('L', 5);
+		
+		assertEquals(player.getPos().toString(), checkPos.toString());
+	}
+	
+	@Test
+	public void moveTestLeftLowercase() {
+		
+		Position startPos = new Position(3, 4);
+		player.setStartingPosition(startPos);		
+		Position checkPosl = new Position(2, 4);
+		player.move('l', 5);
+		
+		assertEquals(player.getPos().toString(), checkPosl.toString());
+	}		
+
+	@Test
+	public void moveTestLeftOutOfBounds() {
+		//Position startPos = new Position(3, 4);
+		
+		Position startPos2 = new Position(0, 4);
+		player.setStartingPosition(startPos2);
+		player.move('L', 5);
+		
+		// check if starting position stayed the same
+		assertEquals(player.getPos().toString(), startPos2.toString());
+	}
+	
+	@Test
+	public void moveTestUpUppercase() {
+		
+		Position startPos = new Position(3, 3);
+		player.setStartingPosition(startPos);
+		Position checkPos = new Position(3, 2);
+		player.move('U', 5);
+		
+		assertEquals(player.getPos().toString(), checkPos.toString());
+	}
+	
+	@Test
+	public void moveTestUpLowercase() {
+		
+		Position startPos = new Position(3, 3);
+		player.setStartingPosition(startPos);		
+		Position checkPosl = new Position(3, 2);
+		player.move('u', 5);
+		
+		assertEquals(player.getPos().toString(), checkPosl.toString());
+	}		
+
+	@Test
+	public void moveTestUpOutOfBounds() {
+		//Position startPos = new Position(3, 4);
+		
+		Position startPos2 = new Position(0, 4);
+		player.setStartingPosition(startPos2);
+		player.move('U', 5);
+		
+		// check if starting position stayed the same
+		assertEquals(player.getPos().toString(), startPos2.toString());
+	}
+	
+	@Test
+	public void moveTestDownUppercase() {
+		
+		Position startPos = new Position(3, 3);
+		player.setStartingPosition(startPos);
+		Position checkPos = new Position(3, 4);
+		player.move('D', 5);
+		
+		assertEquals(player.getPos().toString(), checkPos.toString());
+	}
+	
+	@Test
+	public void moveTestDownLowercase() {
+		
+		Position startPos = new Position(3, 3);
+		player.setStartingPosition(startPos);		
+		Position checkPosl = new Position(3, 4);
+		player.move('d', 5);
+		
+		assertEquals(player.getPos().toString(), checkPosl.toString());
+	}		
+
+	@Test
+	public void moveTestDownOutOfBounds() {
+		//Position startPos = new Position(3, 4);
+		
+		Position startPos2 = new Position(0, 0);
+		player.setStartingPosition(startPos2);
+		player.move('D', 5);
+		
+		// check if starting position stayed the same
+		assertEquals(player.getPos().toString(), startPos2.toString());
+	}
+	
+	@Test
+	public void moveTestRightUppercase() {
+		
+		Position startPos = new Position(3, 4);
+		player.setStartingPosition(startPos);
+		Position checkPos = new Position(4, 4);
+		player.move('R', 5);
+		
+		assertEquals(player.getPos().toString(), checkPos.toString());
+	}
+	
+	@Test
+	public void moveTestRightLowercase() {
+		
+		Position startPos = new Position(3, 4);
+		player.setStartingPosition(startPos);		
+		Position checkPosl = new Position(4, 4);
+		player.move('r', 5);
+		
+		assertEquals(player.getPos().toString(), checkPosl.toString());
+	}		
+
+	@Test
+	public void moveTestRightOutOfBounds() {
+		//Position startPos = new Position(3, 4);
+		
+		Position startPos2 = new Position(4, 4);
+		player.setStartingPosition(startPos2);
+		player.move('R', 5);
+		
+		// check if starting position stayed the same
+		assertEquals(player.getPos().toString(), startPos2.toString());
+	}
+	
+	@Test
+	public void setStartingPositionTest() {
+		
+		player.setStartingPosition(5);
+		Position startPos = player.getStartingPosition();
+		
+		assertEquals(Map.getTiles()[startPos.getX()][startPos.getY()], Color.GREEN);
+	}
+	
+	
+}
